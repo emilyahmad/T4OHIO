3a66779172a5cb7402477886f679e46639a27efb
Emily Ahmad <126031180+emilyahmad@users.noreply.github.com>
2025-12-21 00:51:57 +0000

    working pause/play

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-// create variable & assign to pause button
-const pauseButton = document.getElementById('pause');
-const playButton = document.getElementById('play');
-
+/* Video controls */
+const herovideo = document.getElementById('myVideo');
+const pause = document.getElementById('pauseButton');
+pause.addEventListener('click', pauseVideo);
+const play = document.getElementById('playButton');
+play.addEventListener('click', playVideo);
+// hide play button first
+play.hidden = true;
+// track if playing or not
+function pauseVideo() {
+    pause.hidden = true;
+    herovideo.pause();
+    play.hidden = false;
+}
+function playVideo() {
+    play.hidden = true;
+    herovideo.play();
+    pause.hidden = false;
+}
